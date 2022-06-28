bfa15e852fde2cb45f6717d0576791cf35e87167
Gauthier3090 <gauthier.pladet@gmail.com>
2022-06-28 15:57:46 +0200

    Api request movies

--- MoviesHub/Controllers/FluxController.cs
+++ MoviesHub/Controllers/FluxController.cs
@@ -1,14 +1,16 @@
-﻿using Isopoh.Cryptography.Argon2;
+﻿using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using MoviesHub.Models;
-using MoviesHub.Models.Mappers;
 using MoviesHub_BLL.Services;
+using Newtonsoft.Json;
 
 namespace MoviesHub.Controllers;
 
 public class FluxController : Controller
 {
     private readonly UserService _userService;
+    private readonly string _baseUrl = "https://imdb-api.com/fr/";
+    private readonly string apiKey = "k_8nrxshx3";
 
     public FluxController(UserService userService)
     {
@@ -17,6 +19,43 @@ public class FluxController : Controller
 
     public IActionResult Index()
     {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public async Task<ActionResult> MoviesList()
+    {
+        List<SearchResult> search = new();
+        using HttpClient client = new();
+        client.BaseAddress = new Uri(_baseUrl);
+        client.DefaultRequestHeaders.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        try
+        {
+            SearchData? response = await client.GetFromJsonAsync<SearchData>($"API/Search/{apiKey}/leseigneur");
+            Console.WriteLine(response?.Results);
+            foreach (var movie in response.Results)
+            {
+                search.Add(movie);
+            }
+            return View(search);
+        }
+        catch (HttpRequestException)
+        {
+            Console.WriteLine("An error occurred.");
+        }
+        catch (NotSupportedException)
+        {
+            Console.WriteLine("The content type is not supported.");
+        }
+        catch (JsonException)
+        {
+            Console.WriteLine("Invalid JSON.");
+        }
         return View();
     }
 }
